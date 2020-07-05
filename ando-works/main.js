d29dcf8b657466ba84029802610369c2c95ab89c
Ando Saki <61814934+and1720015@users.noreply.github.com>
2020-07-05 16:02:17 +0900

    Add files via upload

---
+++ ando-works/main.js
@@ -0,0 +1,73 @@
+$(function(){
+	$("#page").hide();
+	$("#open-page").click(function(){
+		$("#page").fadeIn();
+	});
+	$("#close-page").click(function(){
+		$("#page").fadeOut();
+	});
+
+	$("#page2").hide();
+	$("#open-page2").click(function(){
+		$("#page2").fadeIn();
+	});
+	$("#close-page2").click(function(){
+		$("#page2").fadeOut();
+	});
+
+	$("#page3").hide();
+	$("#open-page3").click(function(){
+		$("#page3").fadeIn();
+	});
+	$("#close-page3").click(function(){
+		$("#page3").fadeOut();
+	});
+
+	$("#page4").hide();
+	$("#open-page4").click(function(){
+		$("#page4").fadeIn();
+	});
+	$("#close-page4").click(function(){
+		$("#page4").fadeOut();
+	});
+
+	$("#page5").hide();
+	$("#open-page5").click(function(){
+		$("#page5").fadeIn();
+	});
+	$("#close-page5").click(function(){
+		$("#page5").fadeOut();
+	});
+
+	$("#page6").hide();
+	$("#open-page6").click(function(){
+		$("#page6").fadeIn();
+	});
+	$("#close-page6").click(function(){
+		$("#page6").fadeOut();
+	});
+
+	$("#page7").hide();
+	$("#open-page7").click(function(){
+		$("#page7").fadeIn();
+	});
+	$("#close-page7").click(function(){
+		$("#page7").fadeOut();
+	});
+
+	$("#page8").hide();
+	$("#open-page8").click(function(){
+		$("#page8").fadeIn();
+	});
+	$("#close-page8").click(function(){
+		$("#page8").fadeOut();
+	});
+
+	$("#page9").hide();
+	$("#open-page9").click(function(){
+		$("#page9").fadeIn();
+	});
+	$("#close-page9").click(function(){
+		$("#page9").fadeOut();
+	});
+});
